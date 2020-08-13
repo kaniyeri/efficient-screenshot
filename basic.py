b1b3d65ace86997b8e3bfa92685fc1c58b355a1d
theblueeye-dev <69250757+theblueeye-dev@users.noreply.github.com>
2020-08-13 17:36:08 +0530

    Update basic.py
    Ability to save multiple files now!

--- basic.py
+++ basic.py
@@ -1,8 +1,21 @@
 import keyboard
 from mss import mss
+import datetime
+
+count = 0
+partial_filename = str(datetime.datetime.now())
+partial_filename=partial_filename.replace("." , "")
+partial_filename=partial_filename.replace(" " , "")
+partial_filename=partial_filename.replace(":" , "")
+
 while keyboard.is_pressed('Esc')==False:
 	if keyboard.is_pressed('Ctrl'):
 		with mss() as sct:
-			sct.shot()
+			name = str(partial_filename+str(count)+".png")
+			
+			filename = sct.shot(mon=-1, output=name)
+			print(filename)
+			count+=1
+
 
 	
