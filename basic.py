4591fc0e24b1ca05c45eb4080f2a37ed07906c80
theblueeye-dev <69250757+theblueeye-dev@users.noreply.github.com>
2020-10-02 17:17:16 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,10 +4,12 @@ import datetime
 
 count = 0
 partial_filename = str(datetime.datetime.now())
-partial_filename=partial_filename.replace("." , "")
-partial_filename=partial_filename.replace(" " , "")
-partial_filename=partial_filename.replace(":" , "")
-
+for item in ["." , " " ,":"]:
+	partial_filename=partial_filename.replace(item , "")
+	
+#partial_filename=partial_filename.replace("." , "")
+#partial_filename=partial_filename.replace(" " , "")
+#partial_filename=partial_filename.replace(":" , "")
 while keyboard.is_pressed('Esc')==False:
 	if keyboard.is_pressed('Ctrl'):
 		with mss() as sct:
